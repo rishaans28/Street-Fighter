28594e904bbea79e3b3b45056be5882621579031
rishaans28 <rishaansoni28@gmail.com>
2025-06-19 12:26:43 -0700

    Update sprites.py

--- Code/sprites.py
+++ Code/sprites.py
@@ -3,9 +3,10 @@ from os.path import join
 from custom_timer import Timer
 
 class BasePlayer(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, opponent, idle_img, punch_img, kick_img, duck_size, controls, duck_y, health_bar_y, winner_name, punch_sound, kick_sound):
+    def __init__(self, pos, groups, opponent, player, idle_img, punch_img, kick_img, duck_size, controls, duck_y, health_bar_y, winner_name, punch_sound, kick_sound):
         super().__init__(groups)
         self.opponent = opponent
+        self.player = player
         self.pos = pos
         self.speed = 200
         self.health = 200
@@ -33,6 +34,7 @@ class BasePlayer(pygame.sprite.Sprite):
         self.punch_landed = False
         self.kick_sound = kick_sound
         self.kick_landed = False
+        self.fireball_available = False
 
     def display_health_bar(self, screen):
         if self.health >= 100:
@@ -66,23 +68,26 @@ class BasePlayer(pygame.sprite.Sprite):
         if self.rect.right >= 1350:
             self.rect.right = 1350
 
-    def collisions(self):
+    def collisions(self, dt):
         if pygame.sprite.collide_mask(self, self.opponent) and self.is_attacking:
             if self.image == self.kick_state and self.opponent.image == self.opponent.kick_state:
                 return
             if self.image == self.punch_state and self.opponent.image == self.opponent.punch_state:
                 return
-            if self.image == self.punch_state and not self.punch_landed:
-                self.punch_landed = True
-                self.punch_sound.play()
-            if self.image == self.kick_state and not self.kick_landed:
-                self.kick_landed = True
-                self.kick_sound.play()
-            self.opponent.health -= 1
+            if self.image == self.punch_state:
+                self.opponent.health -= 35 * dt
+                if not self.punch_landed:
+                    self.punch_landed = True
+                    self.punch_sound.play()
+            if self.image == self.kick_state:
+                self.opponent.health -= 50 * dt
+                if not self.kick_landed:
+                    self.kick_landed = True
+                    self.kick_sound.play()
 
-    def input(self, dt):
+    def input(self, dt, group1, group2):
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[self.controls['right']]) - int(keys[self.controls['left']])
+        self.direction.x = int(keys[self.controls["right"]]) - int(keys[self.controls['left']])
         self.direction.y = 0
         if keys[self.controls['kick']] and self.can_attack:
             self.can_attack = False
@@ -103,21 +108,28 @@ class BasePlayer(pygame.sprite.Sprite):
             self.punch_landed = False
             self.reset_timer.activate()
         if keys[self.controls['duck']] and self.can_attack:
-            self.can_attack = False
             self.is_ducking = True
             self.image = self.duck_state
             self.mask = pygame.mask.from_surface(self.image)
             self.rect = self.image.get_frect(center=self.rect.center)
             self.rect.centery = self.duck_y
             self.reset_timer.activate()
+        if keys[self.controls["fireball"]] and self.fireball_available:
+            self.can_attack = False
+            self.fireball_available = False
+            if self.player == "player1":
+                Fireball((group1, group2), (self.rect.right + 180, self.rect.centery - 150), "player1")
+            elif self.player == "player2":
+                Fireball((group1, group2), (self.rect.left - 180, self.rect.centery - 150), "player2")
+            self.reset_timer.activate()
         self.rect.center += self.direction * self.speed * dt
 
-    def update(self, dt, screen):
+    def update(self, dt, screen, group1, group2):
         self.attack_timer.update()
         self.reset_timer.update()
-        self.input(dt)
+        self.input(dt, group1, group2)
         self.boundaries()
-        self.collisions()
+        self.collisions(dt)
         self.display_health_bar(screen)
 
 class Player1(BasePlayer):
@@ -127,10 +139,12 @@ class Player1(BasePlayer):
             'right': pygame.K_d,
             'kick': pygame.K_LSHIFT,
             'punch': pygame.K_q,
-            'duck': pygame.K_s
+            'duck': pygame.K_s,
+            'fireball' : pygame.K_f
         }
         super().__init__(
             pos, groups, player2,
+            player="player1",
             idle_img="ryu_idle_stance.png",
             punch_img="ryu_punch.png",
             kick_img="ryu_kick.png",
@@ -140,7 +154,7 @@ class Player1(BasePlayer):
             health_bar_y=550,
             winner_name="player2",
             punch_sound=punch_sound,
-            kick_sound=kick_sound
+            kick_sound=kick_sound,
         )
 
 class Player2(BasePlayer):
@@ -150,10 +164,12 @@ class Player2(BasePlayer):
             'right': pygame.K_RIGHT,
             'kick': pygame.K_RSHIFT,
             'punch': pygame.K_SPACE,
-            'duck': pygame.K_DOWN
+            'duck': pygame.K_DOWN,
+            'fireball' : pygame.K_SLASH
         }
         super().__init__(
             pos, groups, player1,
+            player="player2",
             idle_img="ken_idle_stance.png",
             punch_img="ken_punch.png",
             kick_img="ken_kick.png",
@@ -165,3 +181,48 @@ class Player2(BasePlayer):
             punch_sound=punch_sound,
             kick_sound=kick_sound
         )
+
+class HealthDrop(pygame.sprite.Sprite):
+    def __init__(self, groups, pos):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("Images", "health.png"))
+        self.rect = self.image.get_frect(midbottom = pos)
+        self.speed = 400
+
+    def update(self, dt, *args, **kwargs):
+        self.rect.centery += self.speed * dt
+        if self.rect.bottom >= 800:
+            self.kill()
+
+class FireballDrop(pygame.sprite.Sprite):
+    def __init__(self, groups, pos):
+        super().__init__(groups)
+        self.image = pygame.image.load(join("Images", "fireballdrop.png"))
+        self.rect = self.image.get_frect(midbottom = pos)
+        self.speed = 400
+    
+    def update(self, dt, *args, **kwargs):
+        self.rect.centery += self.speed * dt
+        if self.rect.bottom >= 800:
+            self.kill()
+
+class Fireball(pygame.sprite.Sprite):
+    def __init__(self, groups, pos, player):
+        super().__init__(groups)
+        self.player = player
+        self.speed = 500
+        self.image = pygame.image.load(join("Images", "fireball.png"))
+        if self.player == "player2":
+            self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_frect(center = pos)
+        self.direction = pygame.Vector2(1,0) if self.player == "player1" else pygame.Vector2(-1,0)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.mask_rect = self.mask.get_bounding_rects()[0].move(self.rect.topleft)
+
+    def update(self, dt, *args, **kwargs):
+        self.mask_rect = self.mask.get_bounding_rects()[0].move(self.rect.topleft)
+        self.rect.centerx += self.direction.x * self.speed * dt
+        if self.mask_rect.left <= 0:
+            self.kill()
+        if self.mask_rect.right >= 1350:
+            self.kill()
